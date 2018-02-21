a82abe2953281d77bf36e5d37dab7cb0fa814a4b
Zaiter <azaiter@svsu.edu>
2018-02-21 18:10:28 -0500

    starting regex stuff

--- regex.java
+++ regex.java
@@ -1,11 +1,8 @@
 (* A list of uppercase and lowercase letters. *)
-letter = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K" | "L" | "M"
-	   | "N" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" | "W" | "X" | "Y" | "Z"
-	   | "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m"
-	   | "n" | "o" | "p" | "q" | "r" | "s" | "t" | "u" | "v" | "w" | "x" | "y" | "z";
+String letter = "(A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z|a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z)";
 
 (* Rules for all numbers. *)
-sign = ["+" | "-"];
+String sign = "((\\+|\\-)+)";
 
 (* Rules for binary numbers. *)
 binary_digit = "0" | "1";
